31d0fdfed51534e566e1d76acb049244343e2a55
Gokhul26 <gokhul2005@gmail.com>
2024-11-13 08:59:18 +0530

    Still the dynamo-db is having some issues with the implementation

--- RemoteBackend/backend.tf
+++ RemoteBackend/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-remote-bucket-12345"
+    bucket = "terraform-remote-bucket-gokhul"
     region = "us-east-1"
     key = "goks/terraform.tfstate"
     dynamodb_table = "lock-terraform-new"
--- RemoteBackend/main.tf
+++ RemoteBackend/main.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "remote-instance" {
     ami = "ami-0e86e20dae9224db8"
 }
 
-resource "aws_s3_bucket" "remoteBucket" {
-    bucket = "terraform-remote-bucket-12345"
-}
+# resource "aws_s3_bucket" "remoteBucket" {
+#     bucket = "terraform-remote-bucket-gokhul-1"
+# }
 resource "aws_dynamodb_table" "terraform_lock" {
     name = "lock-terraform-new"
     hash_key         = "TestTableHashKey"
